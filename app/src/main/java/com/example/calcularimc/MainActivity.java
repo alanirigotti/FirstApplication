0a4432caa0a1b15373fd71857dc9bb9699fdf283
alanirigotti <alani.ro02@gmail.com>
2023-02-27 17:31:03 -0300

    Realizei correção de bugs. Agora o app está pronto.

--- app/src/main/java/com/example/calcularimc/MainActivity.java
+++ app/src/main/java/com/example/calcularimc/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.calcularimc;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textResultado;
-    private EdiText editPeso;
+    private EditText editPeso;
     private EditText editAltura;
 
     @Override
